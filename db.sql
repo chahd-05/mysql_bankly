8c1c6d6e10f7a7e2087ee53a6dbf3f3322f9ecb9
shahd <mazouzchahd12@gmail.com>
2025-12-03 14:34:11 +0100

    create table of advisors

--- db.sql
+++ db.sql
@@ -9,5 +9,10 @@ use bankly;
     -> registration_date date default current_timestamp
     -> );
 
+    create table advisors (
+    -> advisor_id int primary key auto_increment,
+    -> full_name varchar (50),
+    -> email varchar(100)
+    -> );
 
-    
+
