a08acf265dd932021cbb38a32a4e7b9013afea2c
shahd <mazouzchahd12@gmail.com>
2025-12-03 14:32:54 +0100

    create table of customers

---
+++ db.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE bankly;
+use bankly;
+
+    create table customers (
+    -> customer_id int primary key auto_increment,
+    -> full_name varchar (50),
+    -> email varchar (100),
+    -> phone int,
+    -> registration_date date default current_timestamp
+    -> );
+
+
+    
